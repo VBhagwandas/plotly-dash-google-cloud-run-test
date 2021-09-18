99079567644ea0b3e3a3fb22f59d735706ed7432
Vijay Bhagwandas <35529520+VBhagwandas@users.noreply.github.com>
2021-09-18 13:42:20 +0200

    Update custom-script.js

--- assets/custom-script.js
+++ assets/custom-script.js
@@ -1,4 +1,5 @@
+alert('If you see this alert, then your custom JavaScript script has run!')
 <script type="text/javascript">
-	Paddle.Environment.set('sandbox');
+  Paddle.Environment.set('sandbox');
   Paddle.Setup({ vendor: 3137 });
 </script>
