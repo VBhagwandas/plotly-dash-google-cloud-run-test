c7b5f96d67b76853b935a82a7a75bde1a8fd3ab8
Vijay Bhagwandas <35529520+VBhagwandas@users.noreply.github.com>
2021-09-20 14:46:41 +0200

    Update paddle-setup.js

--- assets/paddle-setup.js
+++ assets/paddle-setup.js
@@ -1,2 +1,16 @@
 Paddle.Environment.set('sandbox');
 Paddle.Setup({ vendor: 3137 });
+
+Paddle.Audience.Popup({
+  vendorName: 'Your Company Name',
+  triggers: {
+    exitIntent: false,
+    scrollDepth: false,
+    timed: 10
+  },
+  strings: {
+    heading: "Stay up to date!",
+    subHeading: "Never miss a new feature, release, announcement or deal. Sign up to our newsletter today!",
+    cta: "Sign me up!"
+  }
+});
